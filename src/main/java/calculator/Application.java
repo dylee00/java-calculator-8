05f9f5622224ea59e78a70efa16521d5cd2effce
dylee00 <dylee6820@gmail.com>
2025-10-20 11:09:10 +0900

    feat(input): 커스텀 구분자를 가지는 문자열을 입력하면 해당 구분자를 기준으로 숫자 파싱

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,8 +10,20 @@ public class Application {
         //문자열 입력
         String input = Console.readLine();
 
-        //구분자를 기준으로 숫자 파싱
-        String[] numbers = input.split("[,:]");
+        //기본 구분자, 커스텀 구분자를 기준으로 숫자 파싱
+        String defaultDelimiter = "[,:]";
+
+        if(input.startsWith("//")) {
+            //커스텀 구분자의 길이 구하기
+            int customDelimiterEndIndex = input.indexOf("\\n");
+            //커스텀 구분자를 String 안에서 추출한 다음 defaultDelimiter에 추가
+            String customDelimiter = input.substring(2, customDelimiterEndIndex);
+            defaultDelimiter = "[" + customDelimiter + "]";
+            //커스텀 구분자를 입력한 문자열을 떼고 input 값 갱신
+            input = input.substring(customDelimiterEndIndex + 2);
+
+        }
+        String[] numbers = input.split(defaultDelimiter);
 
         for (String number : numbers) {
             System.out.println(Integer.parseInt(number));
