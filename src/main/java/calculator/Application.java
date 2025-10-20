8abf4409de0a80f9fdb9581fb29ec9babbba2398
dylee00 <dylee6820@gmail.com>
2025-10-20 10:54:05 +0900

    feat(input): , : 구분자로 가지는 문자열을 입력하면 해당 구분자를 기준으로 숫자를 파싱

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        //문자열 입력
+        String input = Console.readLine();
+
+        //구분자를 기준으로 숫자 파싱
+        String[] numbers = input.split("[,:]");
+
+        for (String number : numbers) {
+            System.out.println(Integer.parseInt(number));
+        }
+
     }
 }
